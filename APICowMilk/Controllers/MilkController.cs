85f4e91c1d89fe319715b7f6f87e1bf2ad78be0f
jcvheerden <ST10316473@vcconnect.edu.za>
2025-05-06 09:16:07 +0200

    Docker

---
+++ APICowMilk/Controllers/MilkController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APICowMilk.Controllers
+{
+    public class MilkController : ControllerBase
+    {
+        [HttpGet("GetMeMilk")]
+
+        public IActionResult GetGotMilk()
+        {
+            return Ok("Here is milk :) ");
+        }
+
+    }
+}
